a712c0151f81976f7e99d8d89ed59f6968273e3c
Ersin <michael.knight@cydeo.com>
2026-02-28 10:34:31 +0300

    Created day_01 ClassAndObject package

--- src/test/day01_ClassAndObject/ClassAndObject_Intro.java
+++ src/test/day01_ClassAndObject/ClassAndObject_Intro.java
@@ -1,4 +1,12 @@
 package test.day01_ClassAndObject;
 
 public class ClassAndObject_Intro {
+    public static void main(String[] args) {
+        // Class: blueprint, template, design
+        // Object: instance of a class
+
+        // Class is a template for creating objects. It defines the properties and behaviors that the objects created from the class will have.
+
+        // Object is an instance of a class. It is created based on the class and has its own unique identity and state.
+    }
 }
